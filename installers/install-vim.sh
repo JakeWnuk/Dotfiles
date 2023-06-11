fac462839c478e9992f6ea5bfec496af852e19c5
Jake Wnuk <jakewnuk@gmail.com>
2023-06-11 10:57:11 -0400

    Update install-vim.sh
    Attempt to fix issue where the go executable is not found when installing plugins.

--- installers/install-vim.sh
+++ installers/install-vim.sh
@@ -2,4 +2,9 @@
 
 # Install Vim Plugins
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+# NOTE: Placing this here to source the go binary properly before attempting
+# install
+source ~/.bashrc
+
 echo 'command' | vim +PlugInstall +GoInstallBinaries +qall 2>/dev/null
