87a2ca91a50aa29edb5f2fd87564c28bff603249
Jake Wnuk <jakewnuk@gmail.com>
2023-09-09 19:38:17 -0400

    Update install-grc.sh
    - update for ansible but try not to break the dockerbuild

--- installers/install-grc.sh
+++ installers/install-grc.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
+
 for cmd in g++ gas head make ld ping6 tail traceroute6 $( ls /usr/share/grc/ ); do
   cmd="${cmd##*conf.}"
   type "${cmd}" >/dev/null 2>&1 && echo "alias '${cmd}"="$( which grc) --colour=auto ${cmd}'" >> ~/.grc_aliases
 done
 
-echo '' >> /etc/grc.conf
-echo '# curl' >> /etc/grc.conf
-echo '(^|[/\w\.]+/)g?curl\s?' >> /etc/grc.conf
-echo 'conf.curl' >> /etc/grc.conf
+if command -v sudo > /dev/null; then
+    echo '' | sudo tee -a /etc/grc.conf
+    echo '# curl' | sudo tee -a /etc/grc.conf
+    echo '(^|[/\w\.]+/)g?curl\s?' | sudo tee -a /etc/grc.conf
+    echo 'conf.curl' | sudo tee -a /etc/grc.conf
+else
+    echo '' >> /etc/grc.conf
+    echo '# curl' >> /etc/grc.conf
+    echo '(^|[/\w\.]+/)g?curl\s?' >> /etc/grc.conf
+    echo 'conf.curl' >> /etc/grc.conf
+fi
