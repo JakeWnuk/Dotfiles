9dee06ff708f4f4b8eb9c5d2f388cf7536fbdae9
Jake Wnuk <jakewnuk@gmail.com>
2023-09-08 20:27:48 -0400

    Update install-updates.sh

--- installers/install-updates.sh
+++ installers/install-updates.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 apt-get update -qq
-apt-get install -y tmux jq grc zsh-syntax-highlighting docker.io vim wget curl tree gcc stow
+apt-get install -y tmux jq grc zsh-syntax-highlighting docker.io vim wget curl tree gcc stow python3-pip
 apt-get update --fix-missing
