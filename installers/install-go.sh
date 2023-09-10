102d5851483df07fb811e91b6f092af13999413c
Jake Wnuk <jakewnuk@gmail.com>
2023-09-09 21:03:33 -0400

    bug fixing for ansible
    - rewriting the installer scripts to be flexible enough to install for both docker and ansible

--- installers/install-go.sh
+++ installers/install-go.sh
@@ -2,7 +2,14 @@
 
 URL=https://go.dev/dl/go1.21.1.linux-amd64.tar.gz
 wget -O /tmp/go.linux-amd64.tar.gz $URL
-rm -rf /usr/local/go && \
+
+if command -v sudo > /dev/null; then
+    sudo rm -rf /usr/local/go && \
+    sudo tar -C /usr/local -xzf /tmp/go.linux-amd64.tar.gz && \
+    rm /tmp/go.linux-amd64.tar.gz
+else
+    rm -rf /usr/local/go && \
     tar -C /usr/local -xzf /tmp/go.linux-amd64.tar.gz && \
     rm /tmp/go.linux-amd64.tar.gz
+fi
 
--- installers/install-pipx.sh
+++ installers/install-pipx.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
-python3 -m pip install --user pipx --break-system-packages
+# Check if --break-system-packages option is available
+if python3 -m pip install --help | grep -q -- '--break-system-packages'; then
+    python3 -m pip install --user pipx --break-system-packages
+else
+    python3 -m pip install --user pipx
+fi
+
 python3 -m pipx ensurepath
 
 source ~/.bashrc
