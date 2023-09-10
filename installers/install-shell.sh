c25f1c459ef57f823b78712f9bdc579cd5a6274c
Jake Wnuk <jakewnuk@gmail.com>
2023-09-09 20:46:21 -0400

    Update install-shell.sh

--- installers/install-shell.sh
+++ installers/install-shell.sh
@@ -10,5 +10,11 @@ mkdir ~/.zsh/ && wget -O ~/.zsh/zsh-z.plugin.zsh $URL
 
 # Source and set shell
 source ~/.bashrc; source ~/.zshrc
-echo "/usr/bin/zsh" | sudo tee -a /etc/shells
-sudo chsh -s /usr/bin/zsh $(whoami)
+
+if command -v sudo > /dev/null; then
+    echo "/usr/bin/zsh" | sudo tee -a /etc/shells
+    sudo chsh -s /usr/bin/zsh $(whoami)
+else
+    echo "/usr/bin/zsh" >> /etc/shells
+    chsh -s /usr/bin/zsh $(whoami)
+fi
