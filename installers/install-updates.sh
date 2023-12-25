71798c6425fbd946efb5707c52213c71cac1a21c
Jake Wnuk <jakewnuk@gmail.com>
2023-12-25 18:48:56 -0500

    Update install-tools.sh

--- installers/install-updates.sh
+++ installers/install-updates.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 apt-get update -qq
-apt-get install -y tmux grc zsh-syntax-highlighting docker.io vim wget curl tree gcc stow python3-pip python3-venv nodejs dos2unix iputils-ping rlwrap
+apt-get install -y tmux grc zsh-syntax-highlighting docker.io vim wget curl \
+tree gcc stow python3-pip python3-venv nodejs dos2unix openvpn iputils-ping rlwrap
 apt-get update --fix-missing
