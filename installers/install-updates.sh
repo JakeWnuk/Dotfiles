169023b666dd70d88288bf67c25e7386554cc6fa
Jake Wnuk <jakewnuk@gmail.com>
2023-12-30 15:19:27 -0500

    Update vim and installer
    - update vimrc with new command to change between buffers with shift tab
    - added dnsutils to default image

--- installers/install-updates.sh
+++ installers/install-updates.sh
@@ -3,5 +3,5 @@
 apt-get update -qq
 apt-get install -y tmux grc zsh-syntax-highlighting docker.io vim wget curl \
 tree gcc stow python3-pip python3-venv nodejs dos2unix openvpn iputils-ping \
-rlwrap rubygems ruby-dev
+rlwrap rubygems ruby-dev dnsutils
 apt-get update --fix-missing
