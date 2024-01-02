949286bf7b672a47d86f313f5e19eda3420cbbeb
Jake Wnuk <jakewnuk@gmail.com>
2024-01-02 13:11:46 -0500

    Update install-wordlists.sh

--- installers/install-wordlists.sh
+++ installers/install-wordlists.sh
@@ -2,6 +2,5 @@
 
 # install raft wordlists from Assetnote
 mkdir -p /usr/share/wordlists
-cd /usr/share/wordlists && wget -r --no-parent -A "raft*" -R "index.html*" \\
-https://wordlists-cdn.assetnote.io/data/manual/ -nH -e robots=off
+cd /usr/share/wordlists && wget -r --no-parent -A "raft*" -R "index.html*" https://wordlists-cdn.assetnote.io/data/manual/ -nH -e robots=off
 mv ./data/* ./ && rm -rf ./data
