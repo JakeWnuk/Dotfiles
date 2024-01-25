5280af1cc0f32e45b8bdeff17523ad8c6b491dc2
Jake Wnuk <jakewnuk@gmail.com>
2024-01-25 18:27:52 -0500

    Add new var + tool
    add msf ports and httpx

--- installers/install-tools.sh
+++ installers/install-tools.sh
@@ -6,3 +6,7 @@ apt install -y nmap smbclient smbmap hashcat tcpdump
 # Install Pipx tools
 ~/.local/bin/pipx install git+https://github.com/Pennyw0rth/NetExec
 ~/.local/bin/pipx install impacket
+
+# Install Golang tools
+/usr/local/go/bin/go install -v github.com/projectdiscovery/httpx/cmd/httpx@latest
+
