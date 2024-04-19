ace7c20e5c87a2ab6ef64ae3a8dee32105d86c57
Jake Wnuk <jakewnuk@gmail.com>
2024-04-19 12:08:09 -0400

    cp

--- installers/install-msf.sh
+++ installers/install-msf.sh
@@ -2,5 +2,10 @@
 # This installer will install Metasploit from source
 
 # Fetch source from official repository
-#URL=https://raw.githubusercontent.com/rapid7/metasploit-omnibus/master/config/templates/metasploit-framework-wrappers/msfupdate.erb
-#curl $URL > msfinstall && chmod 755 msfinstall && ./msfinstall
+URL=https://raw.githubusercontent.com/rapid7/metasploit-omnibus/master/config/templates/metasploit-framework-wrappers/msfupdate.erb
+
+# Temp fix to the gpg issue from the official repo
+sed 's/sudo gpg/gpg/g'
+
+# Finish installation
+curl $URL > msfinstall && chmod 755 msfinstall && ./msfinstall
--- installers/install-tools.sh
+++ installers/install-tools.sh
@@ -12,12 +12,4 @@ apt install -y nmap smbclient smbmap tcpdump
 /usr/local/go/bin/go install -v \
     github.com/projectdiscovery/httpx/cmd/httpx@latest
 
-/usr/local/go/bin/go install \
-    github.com/jakewnuk/maskcat@v0.0.1 
-
-/usr/local/go/bin/go install \
-    github.com/jakewnuk/rulecat@v0.0.2
-
-/usr/local/go/bin/go install \
-    github.com/jakewnuk/mode@v0.0.1
-
+# TODO - reimplement other tools
