c16b5408bb7278698daee3be17b6da5662a63a5a
Jake Wnuk <jakewnuk@gmail.com>
2024-04-19 12:31:08 -0400

    Update install-msf.sh
    I cant read

--- installers/install-msf.sh
+++ installers/install-msf.sh
@@ -5,7 +5,6 @@
 URL=https://raw.githubusercontent.com/rapid7/metasploit-omnibus/master/config/templates/metasploit-framework-wrappers/msfupdate.erb
 
 # Temp fix to the gpg issue from the official repo
-sed -i 's/sudo gpg/gpg/g' msfinstall
 
 # Finish installation
-curl $URL > msfinstall && chmod 755 msfinstall && ./msfinstall
+curl $URL | sed 's/sudo gpg/gpg/g' > msfinstall && chmod 755 msfinstall && ./msfinstall
