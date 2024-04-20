173db01e1c91afcdb79f6db72996825ee3ebece8
Jake Wnuk <jakewnuk@gmail.com>
2024-04-19 20:21:58 -0400

    Update install-msf.sh

--- installers/install-msf.sh
+++ installers/install-msf.sh
@@ -4,7 +4,5 @@
 # Fetch source from official repository
 URL=https://raw.githubusercontent.com/rapid7/metasploit-omnibus/master/config/templates/metasploit-framework-wrappers/msfupdate.erb
 
-# Temp fix to the gpg issue from the official repo
-
 # Finish installation
-curl $URL | sed 's/sudo gpg/gpg/g' > msfinstall && chmod 755 msfinstall && ./msfinstall
+curl $URL > msfinstall && chmod 755 msfinstall && ./msfinstall
