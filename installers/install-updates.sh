46678bdf17b57b8990a87258038eb534c73af26f
Jake Wnuk <jakewnuk@gmail.com>
2024-07-07 16:16:40 -0400

    Update install-updates.sh

--- installers/install-updates.sh
+++ installers/install-updates.sh
@@ -27,6 +27,6 @@ DEBIAN_FRONTEND=noninteractive apt-get install -y \
     nginx \
     npm \
     clangd \
-    ntp \
+    ntp 
 apt-get update --fix-missing
 DEBIAN_FRONTEND=noninteractive apt-get install --fix-broken
