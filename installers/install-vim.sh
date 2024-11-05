7d26a90c66e9020936e2e7df18eee50c73ea29af
Jake Wnuk <jakewnuk@gmail.com>
2024-11-04 21:02:05 -0500

    Update install-vim.sh

--- installers/install-vim.sh
+++ installers/install-vim.sh
@@ -9,7 +9,7 @@ source ~/.bashrc
 
 # Install dependencies
 echo 'command' | vim +PlugInstall +GoInstallBinaries +qall 2>/dev/null
-vim -es -c 'CocInstall -sync coc-go coc-html coc-css coc-json coc-clangd coc-python coc-prettier coc-yaml' -c 'qa!'
+vim -es -c 'CocInstall -sync coc-go coc-html coc-css coc-json coc-clangd coc-python coc-prettier coc-yaml coc-tsserver coc-markdownlint coc-perl' -c 'q'
 
 # Install GitHub Copilot
 git clone https://github.com/github/copilot.vim ~/.vim/pack/github/start/copilot.vim
